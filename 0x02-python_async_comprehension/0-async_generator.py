ae3b3fbd53aa5915389237a22266af200d657f15
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-05-07 13:08:51 +0000

    Refactor async_generator function to remove type hint ignore in 0x02-python_async_comprehension/0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,7 +12,7 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """Coroutine that loops 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10.
     """
