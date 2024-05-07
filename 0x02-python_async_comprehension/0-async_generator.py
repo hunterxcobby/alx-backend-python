128d144a8f053be2741211b64cd36063d2d9b4cc
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-05-07 13:05:59 +0000

    Refactor async_generator function to use a fixed range and yield random numbers between 0 and 10 in 0x02-python_async_comprehension/0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,10 +9,11 @@ then yield a random number between 0 and 10. Use the random module.
 
 import random
 import asyncio
+from typing import Generator
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     for i in range(10):
         # Simulate an asynchronous operation using asyncio.sleep
         await asyncio.sleep(1)
         # Yield the current value asynchronously
-        yield random.uniform(i, 10.0)
+        yield random.uniform(0, 10)
